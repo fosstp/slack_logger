fb0ade49382451166cbb067b01e0c8b8dd7502f7
William Wu <william@pylabs.org>
2015-06-20 14:40:39 +0800

    create project skeloton

---
+++ slack_logger/__init__.py
@@ -0,0 +1,21 @@
+from pyramid.config import Configurator
+
+def main(global_config, **settings):
+    """ This function returns a Pyramid WSGI application.
+    """
+    config = Configurator(settings=settings)
+    config.include('pyramid_jinja2')
+
+    # session settings
+    #
+    # using builtin session mechanism
+    #from pyramid.session import SignedCookieSessionFactory
+    #sessions_secret = settings['sessions.secret']
+    #session_factory = SignedCookieSessionFactory(sessions_secret)
+    #config.set_session_factory(session_factory)
+
+    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('home', '/')
+
+    config.scan()
+    return config.make_wsgi_app()
